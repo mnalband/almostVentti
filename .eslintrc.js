6adff7c6fde563da1668380960c9da9e7a5fd5b6
Maryna Nalbandian <maryna.nalbandian@sentinelone.com>
2023-09-25 19:50:09 +0200

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,26 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   extends: 'standard',
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-  }
-}
+    indent: ['error', 2], // Example: enforce 2 spaces for indentation
+    quotes: ['error', 'single'], // Example: enforce single quotes for strings
+    semi: ['error', 'always'], // Example: enforce the use of semicolons
+  },
+};
