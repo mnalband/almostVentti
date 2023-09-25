2b3ae34c6f1c9fd526e777df793e182edd18621f
Maryna Nalbandian <maryna.nalbandian@sentinelone.com>
2023-09-25 15:35:03 +0200

    docs: add docstrings

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
 'use strict';
 
+/**
+ * Helper function to query the DOM and select a single element.
+ * @param {string} selector - The CSS selector for the element to select.
+ * @returns {Element} The selected DOM element.
+ */
 const qs = selector => document.querySelector(selector);
+
+/**
+ * Helper function to query the DOM and select multiple elements.
+ * @param {string} selector - The CSS selector for the elements to select.
+ * @returns {NodeList} A NodeList containing the selected DOM elements.
+ */
 const qsa = selector => document.querySelectorAll(selector);
 
+/**
+ * DOM element selectors.
+ */
 const EL = {
   SCORE: ['#score--0', '#score--1'],
   CURRENT: ['#current--0', '#current--1'],
@@ -17,6 +31,10 @@ const DICE_SIDES = 6;
 
 let scores, currentScore, activePlayer, playing;
 
+/**
+ * Initialize the game.
+ * Sets up initial game state and displays it on the UI.
+ */
 const init = () => {
   scores = [0, 0];
   currentScore = 0;
@@ -35,6 +53,10 @@ const init = () => {
 
 init();
 
+/**
+ * Switches to the other player.
+ * Updates the UI to reflect the change in active player.
+ */
 const switchPlayer = () => {
   qs(`#current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -43,6 +65,10 @@ const switchPlayer = () => {
   EL.PLAYERS.forEach(el => qs(el).classList.toggle('player--active'));
 };
 
+/**
+ * Rolls the dice and updates the current score based on the roll.
+ * Updates the UI to reflect the dice roll and current score.
+ */
 const rollDice = () => {
   if (playing) {
     const dice = Math.trunc(Math.random() * DICE_SIDES) + 1;
@@ -58,6 +84,11 @@ const rollDice = () => {
   }
 };
 
+/**
+ * Holds the current score for the active player.
+ * Updates the total score and checks for a winning condition.
+ * Updates the UI to reflect the changes.
+ */
 const holdScore = () => {
   if (playing) {
     scores[activePlayer] += currentScore;
